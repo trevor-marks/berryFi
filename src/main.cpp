f3995eb051075258e73134a5ea074f7c2c22478a
Caleb Johnson <C4leb.Johnson@gmail.com>
2015-04-03 12:09:02 -0700

    testing...

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,7 @@ int main()
 		if (strncmp(input, "QQ", 2) == 0) input_m = HOLD;
 
 		if (strncmp(input, "exit", 4)==0) flag = true;
+		
 		if (strncmp(input, "quit", 4)==0) flag = true;
 
 		Menu.pushButton(input_b, input_m);
