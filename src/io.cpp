1eee9e068f7784497c26156408bf1dc73c965dd9
Caleb Johnson <C4leb.Johnson@gmail.com>
2015-04-30 14:37:57 -0700

    memory address changed to rPi 2

--- src/io.cpp
+++ src/io.cpp
@@ -8,7 +8,7 @@
 
 
 
-#define BCM2708_PERI_BASE  0x20000000
+#define BCM2708_PERI_BASE  0x3F000000
 #define GPIO_BASE         (BCM2708_PERI_BASE + 0x200000)
 #define PAGE_SIZE         (4*1024)
 #define BLOCK_SIZE        (4*1024)
